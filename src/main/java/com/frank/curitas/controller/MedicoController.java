677a2af394af139304cb0c22e154f04c198d9255
frank mijhael <frankmijhael01@gmail.com>
2026-02-23 21:56:15 -0500

    feat: add paginacion en el get method lista medicos

--- src/main/java/com/frank/curitas/controller/MedicoController.java
+++ src/main/java/com/frank/curitas/controller/MedicoController.java
@@ -6,6 +6,8 @@ import com.frank.curitas.medico.Medico;
 import com.frank.curitas.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,9 +27,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListaMedico> listar(){
-        return repository.findAll().stream()
-                .map(DatosListaMedico::new)
-                .toList();
+    public Page<DatosListaMedico> listar(Pageable paginacion){
+        return repository.findAll(paginacion).map(DatosListaMedico::new);
     }
 }
