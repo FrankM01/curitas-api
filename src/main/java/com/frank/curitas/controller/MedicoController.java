e06df2a4ec3e4dee53583d591fcd09b5bcd808ff
frank mijhael <frankmijhael01@gmail.com>
2026-02-23 21:34:22 -0500

    feat: add get method to lista medicos

--- src/main/java/com/frank/curitas/controller/MedicoController.java
+++ src/main/java/com/frank/curitas/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package com.frank.curitas.controller;
 
+import com.frank.curitas.medico.DatosListaMedico;
 import com.frank.curitas.medico.DatosRegistroMedico;
 import com.frank.curitas.medico.Medico;
 import com.frank.curitas.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,12 @@ public class MedicoController {
     @PostMapping
     public void registrar(@RequestBody @Valid DatosRegistroMedico datos){
         repository.save(new Medico(datos));
+    }
+
+    @GetMapping
+    public List<DatosListaMedico> listar(){
+        return repository.findAll().stream()
+                .map(DatosListaMedico::new)
+                .toList();
     }
 }
