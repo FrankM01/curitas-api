5109c347329075f7a09b3c16604abfcc7ada15a4
Frank Bendezu <frankmijhael01@gmail.com>
2026-02-28 21:40:09 -0500

    feat: testeando el caso para un cod 200

--- src/test/java/com/frank/curitas/controller/ConsultaControllerTest.java
+++ src/test/java/com/frank/curitas/controller/ConsultaControllerTest.java
@@ -1,25 +1,46 @@
 package com.frank.curitas.controller;
 
+import com.frank.curitas.domain.consulta.DatosDetalleConsulta;
+import com.frank.curitas.domain.consulta.DatosReservaConsulta;
+import com.frank.curitas.domain.consulta.ReservaConsultas;
+import com.frank.curitas.domain.medico.Especialidad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<DatosReservaConsulta> datosReservaConsultaJson;
+
+    @Autowired
+    private JacksonTester<DatosDetalleConsulta> datosDetalleConsultaJson;
+
+    @MockitoBean
+    private ReservaConsultas reservaConsultas;
 
     @Test
     @DisplayName("Deberia devolver http 400 cuando la requests no tenga datos")
@@ -29,5 +50,30 @@ class ConsultaControllerTest {
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deberia devolver http 200 cuando la requests reciba un json valido")
+    @WithMockUser
+    void reservar_escenario2() throws Exception {
+        var fecha = LocalDateTime.now().plusHours(1);
+        var especialidad = Especialidad.CARDIOLOGIA;
+        var datosDetalle = new DatosDetalleConsulta(null, 2l, 5l, fecha);
+        when(reservaConsultas.reservar(any())).thenReturn(datosDetalle);
+
+        var response = mockMvc.perform(post("/consultas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(datosReservaConsultaJson.write(
+                                new DatosReservaConsulta(2l, 5l, fecha, especialidad)
+                        ).getJson()))
+                .andReturn().getResponse();
+
+
+        var jsonEsperado = datosDetalleConsultaJson.write(
+                datosDetalle
+        ).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
